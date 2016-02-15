a02316039afbcbb473ef6a9e1473947f4eea3009
Vincent The <vincent.s.the@gmail.com>
2016-02-15 09:49:00 +0700

    add remove button

--- public/assets/js/pengadaan-add.js
+++ public/assets/js/pengadaan-add.js
@@ -6,14 +6,21 @@
     atkOptionString += "<option value='" + window.atkList[i].id + "'>" + window.atkList[i].jenis + "</option>";
   }
 
-  var itemRow = "<tr>" +
+  var itemRow = "<tr class='item-row'>" +
     "<td><select class='form-control' name='atk_id[]'>" +
       atkOptionString +
     "</select></td>" +
     "<td><input type='text' name='jumlah_item[]' class='form-control'></td>" +
+    "<td><a class='remove-button'><span class='glyphicon glyphicon-remove'></span></a></td>" +
     "</tr>";
 
   $("#add-item-button").click(function () {
     $("#pengadaan-table tbody").append($(itemRow));
+
+    $(".remove-button").unbind();
+    $(".remove-button").click(function () {
+      console.log($(this));
+      $(this).parents(".item-row").remove();
+    });
   });
 })();
