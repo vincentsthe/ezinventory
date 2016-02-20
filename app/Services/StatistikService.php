40ce1509862c35a1627ad5e935c669bb1659e082
David <davidkwan95@users.noreply.github.com>
2016-02-20 21:54:54 +0700

    Re-added getPemakaianAtk()

--- app/Services/StatistikService.php
+++ app/Services/StatistikService.php
@@ -8,6 +8,21 @@ use App\Models\Atk;
 
 class StatistikService
 {
+	public function getPemakaianAtk($atkId, $startDate, $endDate){
+		$itemCount = 0;
+		$allPemakaian = ItemPemakaian::join('pemakaian', 'pemakaian.id', '=', 'item_pemakaian.pemakaian_id')
+                                        ->where('item_pemakaian.atk_id', '=', $atkId)
+                                        ->where('pemakaian.booking', '=', '0')
+                                        ->where('pemakaian.tanggal_pemakaian', '>=', $startDate)
+                                        ->where('pemakaian.tanggal_pemakaian', '<=', $endDate)
+                                        ->get();
+  
+        foreach ($allPemakaian as $pemakaian) {
+            $itemCount += $pemakaian->jumlah;
+        }
+     
+        return $itemCount;
+	
 
 	public function getStokMinimumAtk(Atk $atk, $startDate, $endDate){
 		$stokCount = 0;
